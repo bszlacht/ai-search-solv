174ce1ea429f3cfda0ac322b0c85f6645a283e21
Kollbi <lkollbek@gmail.com>
2021-09-10 02:18:20 +0200

    New folder for n-puzzle problem

---
+++ problems/n_puzzle/n_puzzle_problem.py
@@ -0,0 +1,22 @@
+from utils import Graph
+
+#TODO: Zdefiniować problem
+#TODO: Typowanie
+
+class nPuzzleProblem():
+    def __init__(self, *args):
+        pass
+    
+    def __str__(self) -> str:
+        pass
+
+    """ 
+    Chcemy mieć początkowy `stan` od razu? Czy najpierw startową konfigurację, która przejdzie w stan?
+    Nazewnictwo: startState lub startConfig (?)
+    """
+    
+    def startState(self):
+        pass
+        
+    def goalState(self):
+        pass
